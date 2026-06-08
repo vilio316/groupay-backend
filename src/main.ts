11769011bbba42dab4ba072f7b5f19b3e8ddcc37
David Atem <atembarry12@gmail.com>
2026-06-08 19:22:13 +0100

    updated: added server docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
   });
+  const docsConfig = new DocumentBuilder()
+    .setTitle('Groupay Docs')
+    .setVersion('0.1.0')
+    .setDescription('The Groupay API Spec.')
+    .addTag('groupay')
+    .build();
+  const docFact = () => SwaggerModule.createDocument(app, docsConfig);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
     origin: ['http://localhost:9909', 'http://localhost:3000'],
@@ -13,6 +21,8 @@ async function bootstrap() {
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
   });
+
+  SwaggerModule.setup('docs', app, docFact);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
